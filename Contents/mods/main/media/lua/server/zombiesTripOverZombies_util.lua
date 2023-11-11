b1d6a95d494d031ff75afd922f9ad97a83f7113d
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2023-11-11 18:25:53 -0500

    zombies won't trip if too close + cooldown for falling

--- Contents/mods/main/media/lua/server/zombiesTripOverZombies_util.lua
+++ Contents/mods/main/media/lua/server/zombiesTripOverZombies_util.lua
@@ -1,5 +1,9 @@
 local util = {}
 
+util.badStates = {
+    [ZombieGetUpState.instance()] = true
+}
+
 function util.isVeryClose(objA,objB,dist)
     dist = dist or 0.3
     return (math.abs(objA:getX()-objB:getX())<=dist and math.abs(objA:getY()-objB:getY())<=dist)
@@ -9,7 +13,7 @@ end
 ---@param otherZombie IsoZombie|IsoGameCharacter|IsoMovingObject|IsoObject
 function util.isValidTripper(character, otherZombie)
     local otherZombieState = otherZombie:getCurrentState()
-    local ozStateValid = (otherZombie:isOnFloor() or otherZombieState == ZombieGetUpState.instance())
+    local ozStateValid = (otherZombie:isOnFloor() or util.badStates[otherZombieState])
     return ( ozStateValid and ZombieOnGroundState.isCharacterStandingOnOther(character, otherZombie) )
 end
 
--- Contents/mods/main/media/lua/server/zombiesTripOverZombies_zombie.lua
+++ Contents/mods/main/media/lua/server/zombiesTripOverZombies_zombie.lua
@@ -2,22 +2,26 @@ local zombiesTripOverZombies = {}
 
 local util = require("zombiesTripOverZombies_util")
 
+zombiesTripOverZombies.timeStamps = {}
 
 ---@param zombie IsoZombie|IsoGameCharacter|IsoMovingObject|IsoObject
 function zombiesTripOverZombies.trip(zombie)
-    if zombie:getBumpedChr() or zombie:isOnFloor() or zombie:getCurrentState()==ZombieGetUpState.instance() then return end
+
+    local lastTripped = zombiesTripOverZombies.timeStamps[zombie]
+    if lastTripped and lastTripped > getTimestampMs() then return end
+    zombiesTripOverZombies.timeStamps[zombie] = getTimestampMs()+200
+
+    if zombie:getBumpedChr() or zombie:isOnFloor() or util.badStates[zombie:getCurrentState()] then return end
 
     if (ZombRand(101) >= SandboxVars.ZombiesTripOverZombies.zombieTripChance) then return end
 
-    if zombie:getSpeedMod() < 0.6 then
+    if zombie:getMoveSpeed() >= 0.06 then
         zombie:setBumpType("trippingFromSprint")
     else
         if SandboxVars.ZombiesTripOverZombies.sprintersOnly == true then return end
         zombie:knockDown(true)
     end
 end
-
-
 
 --for OnZombieUpdate event (zombie)
 ---@param zombie IsoZombie|IsoGameCharacter|IsoMovingObject|IsoObject
@@ -26,6 +30,10 @@ function zombiesTripOverZombies.update(zombie)
     ---@type IsoGridSquare
     local square = zombie:getSquare()
     if not square then return end
+
+    local target = zombie:getTarget()
+
+    if target and util.isVeryClose(zombie,target,0.66) then return end
 
     local bodyHere = square:getDeadBody()
     if bodyHere then if util.isVeryClose(zombie,bodyHere) then zombiesTripOverZombies.trip(zombie) end end
